1ea46b30088d73782b81085f226dd3854e93abdd
Felopssauro <felopsaugusto@gmail.com>
2025-09-27 19:02:52 -0300

    initial commit

---
+++ src/main/java/com/uepa/biblioteca/java/BibliotecaJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.uepa.biblioteca.java;
+
+/**
+ *
+ * @author felops
+ */
+public class BibliotecaJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
